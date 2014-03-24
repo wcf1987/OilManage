5ef531d8423b9e1f43093f31c4b58fb44ef7acb8
icefish <181712814@qq.com>
2014-03-24 17:07:42 +0800

    new db bak

---
+++ OilManage/src/cn/edu/cup/manage/business/AlgorithmJarPlug.java
@@ -0,0 +1,6 @@
+package cn.edu.cup.manage.business;
+
+public interface AlgorithmJarPlug {
+	public String startCalc();
+	public 
+}
---
+++ OilManage/src/cn/edu/cup/manage/business/AlgorthmCalc.java
@@ -0,0 +1,5 @@
+package cn.edu.cup.manage.business;
+
+public class AlgorthmCalc {
+	
+}
