bb5c1aeff403e3a761d90dead400add3597fc3d8
zsy <zsy@zsy-PC>
2014-07-31 16:16:48 +0800

    控件类型管理 提交1

---
+++ OilManage/src/cn/edu/cup/manage/action/GUIPointTypeAction.java
@@ -0,0 +1,144 @@
+package cn.edu.cup.manage.action;
+
+import java.util.Arrays;
+import java.util.List;
+
+import cn.edu.cup.gui.business.PointProper;
+import cn.edu.cup.gui.business.PointType;
+import cn.edu.cup.gui.dao.GUIDao;
+import cn.edu.cup.manage.business.Parameters;
+import cn.edu.cup.manage.dao.GUIPointTypeDao;
+import cn.edu.cup.manage.dao.ParameterDao;
+
+public class GUIPointTypeAction {
+	private int page;
+	private int records;
+	private int rows;
+	private int rowNum;
+	private int total;
+	private String sidx;
+	private String sord;
+	private List<Integer> ids;
+	private List<PointType> pointTypeList;
+
+	public int getPage() {
+		return page;
+	}
+	public void setPage(int page) {
+		this.page = page;
+	}
+	public int getRecords() {
+		return records;
+	}
+	public void setRecords(int records) {
+		this.records = records;
+	}
+	public int getRows() {
+		return rows;
+	}
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+	public int getRowNum() {
+		return rowNum;
+	}
+	public void setRowNum(int rowNum) {
+		this.rowNum = rowNum;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	public String getSidx() {
+		return sidx;
+	}
+	public void setSidx(String sidx) {
+		this.sidx = sidx;
+	}
+	public String getSord() {
+		return sord;
+	}
+	public void setSord(String sord) {
+		this.sord = sord;
+	}
+	public List<Integer> getIds() {
+		return ids;
+	}
+	public void setIds(List<Integer> ids) {
+		this.ids = ids;
+	}
+	
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	
+	public String listPointType(){		
+
+		GUIPointTypeDao dao=new GUIPointTypeDao();
+		pointTypeList=dao.getPointTypeList(page,rows,sidx,sord);
+		records=dao.getCountPointType();
+		if(records!=0&&rows!=0){
+			total=records/rows;
+			if(records%rows!=0){
+				total++;
+			}
+		}
+		dao.close();
+		return "SUCCESS";
+	}
+	int ID;
+	String type;
+	String remark;
+	String path;
+	
+	public List<PointType> getPointTypeList() {
+		return pointTypeList;
+	}
+	public void setPointTypeList(List<PointType> pointTypeList) {
+		this.pointTypeList = pointTypeList;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public String addPointType(){		
+		GUIPointTypeDao dao=new GUIPointTypeDao();		
+		ID=dao.addType(this.type,this.remark,this.path);
+		dao.close();
+		return "SUCCESS";
+	}
+	public String delPointType(){		
+
+		GUIPointTypeDao dao=new GUIPointTypeDao();
+		//AlgorithmInputDao inputDao=new AlgorithmInputDao();
+		if(!ids.isEmpty()){
+
+			for(int id:ids){
+				dao.deleteType(id);
+			}
+		}
+		//inputDao.close();
+		dao.close();
+//		dao.deleteAlgorithm(ID);
+		return "SUCCESS";
+	}
+}
---
+++ OilManage/src/cn/edu/cup/manage/dao/GUIPointTypeDao.java
@@ -0,0 +1,117 @@
+package cn.edu.cup.manage.dao;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import cn.edu.cup.gui.business.GUIPro;
+import cn.edu.cup.gui.business.PointProper;
+import cn.edu.cup.gui.business.PointType;
+import cn.edu.cup.gui.business.PointValue;
+import cn.edu.cup.manage.business.Measure;
+import cn.edu.cup.manage.dao.PhysicalDao;
+import cn.edu.cup.tools.HibernateSessionManager;
+
+public class GUIPointTypeDao {
+
+	 
+	 //SessionFactory sessionFactory;
+	 Session session ;
+	 Transaction tx ;
+		
+
+	public GUIPointTypeDao()
+	{	
+	 
+               
+		//sessionFactory = new Configuration().configure().buildSessionFactory();
+		session = HibernateSessionManager.getThreadLocalSession();
+		
+	}
+
+
+	
+	public List<PointType> getPointTypeList(int page, int rows,
+			String sidx, String sord) {
+		
+		
+		 SQLQuery q = session.createSQLQuery("select t.id,t.type,t.remark,t.path from t_guipointtype t  order by t."+sidx+" "+sord);
+
+			q.setFirstResult((page-1)*rows);
+			q.setMaxResults(rows);
+		
+		
+
+		List l = q.list();
+		List<PointType> re=new ArrayList<PointType>();
+		for(int i=0;i<l.size();i++)
+		{
+			//TestDb user = (TestDb)l.get(i);
+			//System.out.println(user.getUsername());
+
+			  Object[] row = (Object[])l.get(i);;
+			  Integer id = ((Integer)row[0]);
+			  String type=((String)row[1]);
+			  String remark=((String)row[2]);
+			  String path=((String)row[3]);
+	
+			  PointType p=new PointType(id,type,remark,path);
+			  
+			  
+			  re.add(p);
+		}
+	
+		return re;
+	}
+	public int getCountPointType() {
+		String sql="select count(*) from t_guipointtype t ";
+		SQLQuery q = session.createSQLQuery(sql);
+		
+		Integer count=((BigInteger)q.uniqueResult()).intValue();
+		return count;
+
+	}
+	
+	public int addType(String type,String remark, String path) {
+		tx = HibernateSessionManager.getThreadLocalTransaction();
+		Query q = session.createSQLQuery("insert into t_guipointype (type,remark,path) values (?,?,?)");
+		q.setParameter(0, type);
+		q.setParameter(1, remark);
+		q.setParameter(2, path);
+
+		int result=q.executeUpdate();
+
+		return 0;
+	}
+	public int deleteType(int id) {
+		tx = HibernateSessionManager.getThreadLocalTransaction();
+		// TODO Auto-generated method stub
+		SQLQuery q = session.createSQLQuery("delete from t_guipointtype where ID=?");
+		q.setParameter(0, id);
+		int re=q.executeUpdate();
+//		tx.commit();
+		return re;
+	}
+
+	public void close() {
+		// TODO Auto-generated method stub
+		HibernateSessionManager.commitThreadLocalTransaction();
+		HibernateSessionManager.closeThreadLocalSession();
+	}
+
+	public void roll() {
+		// TODO Auto-generated method stub
+		HibernateSessionManager.rollbackThreadLocalTransaction();
+		HibernateSessionManager.closeThreadLocalSession();
+	}
+
+
+
+
+}
