c2644ba7f79cdae9fcfdfbd387bd1b49235578aa
zsy <zsy@zsy-PC>
2014-12-08 19:03:39 +0800

    页面project

--- OilManage/WebRoot/js/project.js
+++ OilManage/WebRoot/js/project.js
@@ -898,6 +898,10 @@ function openProject(proid,algID){
 		sheetgrid.creategrid(proid, sheetDiv+i, pageDiv+i,delID+i,gridWidth);
 		
 	}
-	showTab('input_tab');	
+	var curl=window.location.pathname;
+	if(curl!="/OilManage/pages/simulate_wellbore.jsp"){
+		showTab('input_tab');	
+	}
+	
 	$(".modal").modal('hide');
 }
